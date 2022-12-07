4c82f44f10e4c9dd2de1c41fdb959aa1b441006b
Orz3ch <konrad.orzeszek@gmail.com>
2022-12-07 19:31:07 +0100

    adding new spinner & base error handling

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,16 +1,22 @@
 <template>
+	<base-dialog :show="!!error">
+		<p>{{ error }}</p>
+	</base-dialog>
 	<section>
 		<coach-filter @change-filter="setFilter"></coach-filter>
 	</section>
 	<section>
 		<base-card>
 			<div class="controls">
-				<base-button mode="outline">Refresh</base-button>
-				<base-button v-if="!isCoach" link to="/register"
+				<base-button mode="outline" @click="loadCoaches">Refresh</base-button>
+				<base-button v-if="!isCoach && !isLoading" link to="/register"
 					>Register as Coach</base-button
 				>
 			</div>
-			<ul v-if="hasCoaches">
+			<div v-if="isLoading">
+				<base-spinner></base-spinner>
+			</div>
+			<ul v-else-if="hasCoaches">
 				<coach-item
 					v-for="coach in filteredCoaches"
 					:key="coach.id"
@@ -35,6 +41,8 @@
 		components: { CoachItem, CoachFilter },
 		data() {
 			return {
+				isLoading: false,
+				error: null,
 				activeFilters: {
 					frontend: true,
 					backend: true,
@@ -59,15 +67,27 @@
 				});
 			},
 			hasCoaches() {
-				return this.$store.getters["coaches/hasCoaches"];
+				return !this.isLoading && this.$store.getters["coaches/hasCoaches"];
 			},
 			isCoach() {
 				return this.$store.getters["coaches/isCoach"];
 			},
 		},
+		created() {
+			this.loadCoaches();
+		},
 		methods: {
 			setFilter(updatedFilters) {
 				this.activeFilters = updatedFilters;
+			},
+			async loadCoaches() {
+				this.isLoading = true;
+				try {
+					await this.$store.dispatch("coaches/loadCoaches");
+				} catch (error) {
+					this.error = error.message || "Something went wrong";
+				}
+				this.isLoading = false;
 			},
 		},
 	};
